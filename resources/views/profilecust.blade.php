95bcc475eae1dc6b2d162fb5189718bade5a6ae5
DivadanAr <divadan.arya12@gmail.com>
2022-11-27 18:14:22 +0700

    revisi profile customer

--- resources/views/profilecust.blade.php
+++ resources/views/profilecust.blade.php
@@ -6,57 +6,102 @@
 
 @section('content')
 
-<div class="content">
-  <div class="pict">
-<img src="assets/Rectangle 173.png" alt="">
-<button>Change Photo</button>
-</div>
+<div class="container-top">
+        
+    <div class="content">
+        <div class="pict">
+            <img src="assets/ALM00016.JPG"alt="">
 
-<div class="details">
-<table>
-    <tr>
-        <td>Customer Id</td>
-        <td>
-            <input type="text" value="3241263" disabled class="id">
-        </td>
-    </tr>
-    <tr>
-        <td>First Name</td>
-        <td>
-            <input type="text" value="Kiagus">
-        </td>
-    </tr>
-    <tr>
-        <td>Last Name</td>
-        <td>
-            <input type="text" value="Ahmad">
-        </td>
-    </tr>
-    <tr>
-        <td>Email</td>
-        <td>
-            <input type="email" value="kiagus@gmail.com">
-        </td>
-    </tr>
-    <tr>
-        <td>Phone Number</td>
-        <td>
-            <input type="number" value="0987654432">
-        </td>
-    </tr>
-    <tr>
-        <td class="alamat">Address</td>
-        <td>
-            <textarea  name="" id="" cols="65" rows="5">dihatimu</textarea>
-        </td>
-    </tr>
-</table>
-<div class="pass">
-    <p>Password</p>
-    <input type="password" value="agusganteng"> 
-    <p class="change-pass">Change Password</p>
-</div>
-<button>Save Changes</button>
-</div>
-</div>
+            <a href="{{ Route('editprofile')}}">
+                <button>Edit Profile</button>
+            </a>
+        </div>
+
+        <div class="details">
+            <div class="id">
+                <p>Customer Id</p>
+                <p>24134632</p>
+            </div>
+            <div class="first-name">
+                <p>First Name</p>
+                <p>Agus</p>
+            </div>
+            <div class="last-name">
+                <p>Last Name</p>
+                <p>Ki</p>
+            </div>
+            <div class="email">
+                <p>Email</p>
+                <p>K.g.shadeh@gmail.com</p>
+            </div>
+            <div class="phone-number">
+                <p>Phone Number</p>
+                <p>+6287654321</p>
+            </div>
+            <div class="address">
+                <p>Address</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore adipisci reprehenderit, officia
+
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="pesanan">
+        <div class="dropdown">
+            <select name="" id="">
+                <option value="">All Order</option>
+                <option value="">My Order</option>
+                <option value="">completed Order</option>
+                <option value="">canceled Order</option>
+            </select>
+          </div>
+    </div>
+
+    </div>
+
+    <div class="orderan-gue">
+        <div class="row1">
+            <div class="store">
+                <iconify-icon id="logo" icon="ic:round-store-mall-directory"></iconify-icon>
+                <p>Aldo Mart</p>
+            </div>
+            <p class="total">Total</p>
+        </div>
+
+        <div class="kontencuy">
+            <div class="produknya">
+
+                <img src="assets/web4.jpg" alt="">
+            </div>
+            <div class="detail-produk">
+                <div class="nama-status">
+                    <p>Obat Patah Hati</p>
+                    <div class="statusnya">Confirmed</div>
+
+                </div>
+                <div class="tanggal">
+                    <p>22-22-2222</p>
+                </div>
+                <div class="jumlah-harga">
+                    <p>1x Rp.2000000000</p>
+                </div>
+                <div class="status">
+                    <p>Stuff Received</p>
+                </div>
+            </div>
+            <div class="row-kanan">
+                    <div class="harga-total">
+            <p>Rp.50.000,00</p>
+        </div>
+
+        <div class="btn-pesanan">
+            <button>Buy More</button>
+            <button>Detail</button>
+        </div>
+            </div>
+             
+        </div>
+
+   
+    </div>
 @endsection
