6590470e470395af64fb4aa8edeab7f38c1adb3b
Adri-Bagas <abagaswitjaksono@gmail.com>
2022-12-05 11:14:52 +0700

    cart fixes

--- app/Http/Controllers/HControllerr.php
+++ app/Http/Controllers/HControllerr.php
@@ -5,14 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HControllerr extends Controller
 {
     public function index(){
 
         $products = Product::with('category')->get();
+        $favorites = [0];
 
-        return view('home', compact('products'));
+        if(Auth::check()){
+            foreach(Auth()->user()->favorites as $favorite){
+                array_push($favorites, $favorite->products_id);
+            }
+        }
+
+        return view('home', compact('products', 'favorites'));
     }
 
     public function getProducts(){
